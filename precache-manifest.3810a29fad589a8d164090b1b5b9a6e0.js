7e25bef9d5f241eadce1577ca1f45e437797d203
sanatanghosh <sanatanghosh1997@gmail.com>
2020-07-10 22:03:32 +0530

    Updates

---
+++ precache-manifest.3810a29fad589a8d164090b1b5b9a6e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6163205d5fa872006953e72c3a8c8167",
+    "url": "/food-recipe-app/index.html"
+  },
+  {
+    "revision": "01595b6710ded9dee6a9",
+    "url": "/food-recipe-app/static/css/main.6c1cb729.chunk.css"
+  },
+  {
+    "revision": "dcd58eaaa7c1533c763d",
+    "url": "/food-recipe-app/static/js/2.e73a7eb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe-app/static/js/2.e73a7eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01595b6710ded9dee6a9",
+    "url": "/food-recipe-app/static/js/main.66f461d7.chunk.js"
+  },
+  {
+    "revision": "641f62268cf0dd5f4f6f",
+    "url": "/food-recipe-app/static/js/runtime-main.82c889a9.js"
+  }
+]);
